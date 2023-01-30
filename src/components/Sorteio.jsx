a0915014f31ab91fc9b3639311b22ba2f4f84480
Rodolfo P. Grião <103333634+RodolfoPG@users.noreply.github.com>
2023-01-30 12:39:19 -0300

    Conectando Componentes com Redux #01

--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Card from './Card'
 
-export default props => {
-  
+function Sorteio(props) {
+  const {min, max} = props
   return (
     <Card title="Sorteio de um Número" purple>
       <div>
         <span>
           <span>Resultado: </span>
-          <strong>{10}</strong>
+          <strong>{parseInt(Math.random() * (max - min)) + min}</strong>
         </span>
       </div>
     </Card>
   )
 }
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max
+  }
+}
+
+export default connect(mapStateToProps)(Sorteio)
